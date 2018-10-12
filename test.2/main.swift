11f8bae5f4868817af7123762c85df962b1a7c39
cy1997cj <1418296577@qq.com>
2018-10-12 20:01:12 +0800

    Initial Commit
    从一加到一百

---
+++ test.2/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  test.2
+//
+//  Created by s20171106522 on 2018/10/12.
+//  Copyright © 2018年 s20171106522. All rights reserved.
+//
+
+import Foundation
+var i=1
+var sum=0
+while i<101
+{
+    sum=sum+i
+    i=i+1
+}
+print("从一加到一百的：\(sum)")
+
